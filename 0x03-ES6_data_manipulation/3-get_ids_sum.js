a6cce362eb5d2ddd897f89bb4c0d5c575b511437
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 23:51:26 +0200

    6-set.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,5 +1,3 @@
 export default function getStudentIdsSum(data) {
-    const sumIds = data.reduce(
-        (acc, curr) => curr.id + acc, 0);
-    return sumIds;
+    return data.reduce((acc, curr) => curr.id + acc, 0);
 }
