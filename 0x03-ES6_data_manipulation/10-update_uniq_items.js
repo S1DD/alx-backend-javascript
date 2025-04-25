010b3f2c7edb9a46628eee809a70a9b7c4e4071a
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 23:02:14 +0200

    Fixed: Implementation for updating the values if the condition is true

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -2,9 +2,10 @@ export default function updateUniqueItems(itemMap) {
     if (!(itemMap instanceof Map)) {
         throw new Error('Cannot process');
     }
-    for (const [k, v] of itemMap.entries()) {
-    if (v === 1) {
-        itemMap.set(k, 100);
-    }
-}
+    itemMap.forEach((k, v) => {
+        if (v === 1) {
+            itemMap.set(k, 100)
+        }
+
+    });
 }
