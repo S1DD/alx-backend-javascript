544de14462a872d51a54e5bfeadf6afe5aa5b2a6
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 23:32:09 +0200

    0-get_list_students.js

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,23 @@
+
 export default function getListStudents() {
-    return [
-        {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-        {id: 2, firstName:'James', location: 'Columbia'},
-        {id: 5, firstName:'Serena', location: 'San Francisco'}
-    ];
+   
+    const s1 = {
+        id: 1,
+        firstName: 'Guillaume',
+        location: 'San Francisco',
+      };
+    
+      const s2 = {
+        id: 2,
+        firstName: 'James',
+        location: 'Columbia',
+      };
+    
+      const s3 = {
+        id: 5,
+        firstName: 'Serena',
+        location: 'San Francisco',
+      };
+    
+      return [o1, o2, o3];
 }
