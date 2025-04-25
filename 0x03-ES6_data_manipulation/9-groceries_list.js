2f7e2b09557aff0bef0dadd272b16bb43ea34925
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 23:24:38 +0200

    Corrected implementation of groceriesList function

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,11 +1,11 @@
-export default function groceryList(name, quantity) {
-    return new Map(
-        [
-            ["Apples", 10],
-            ["Tomatoes", 10],
-            ["Pasta", 1],
-            ["Rice", 1],
-            ["Banana", 5],
-        ],
-    );
-}
+export default function groceriesList() {
+    const values = [
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5],
+    ];
+  
+    return new Map(values);
+  }
