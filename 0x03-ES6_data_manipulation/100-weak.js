71b64ad1c0ee36129f09c9f7b1b490e72b185ab5
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 22:56:06 +0200

    Corrected assignment operation

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,20 +1,6 @@
-/**
- * A weak map of endpoints and the number of calls made
- */
 export const weakMap = new WeakMap();
 
-/**
- * Maximum nuber of calls for an endpoint
- */
 const MAX_ENDPOINT_CALLS = 5;
-
-/**
- * Keeps track of the number of calls made to an API's endpoint.
- * @param {{
- *  protocol: String,
- *  name: String,
- * }} endpoint - The endpoint to make a request to. 
- */
 
 export default function queryAPI(endpoint) {
     if (!weakMap.has(endpoint)) {
