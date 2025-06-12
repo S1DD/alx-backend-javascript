e9052e88fe454d101db0e7f5bb8efaae43451d2f
Thabiso M <thabisomolefe515@gmail.com>
2025-06-12 21:36:55 +0000

    Fixed logic

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,13 +1,15 @@
 process.stdout.write("Welcome to ALX, what is your name?\n");
 
-process.stdin.on('readable', () => {
-    const buffer = process.stdin.read();
-
-    if (buffer) {
-        process.stdout.write(`Your name is: ${buffer}`);
-    } 
-});
-
-process.stdin.on('end', () => {
-    process.stdout.write("This is important software is now closing\n");
-});
+if (process.stdin.isTTY) {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+} else {
+  process.stdin.on('data', (data) => {
+    process.stdout.write(`Your name is: ${data.toString()}`);
+    process.exit();
+  });
+  process.on('exit', () => {
+    process.stdout.write('This important software is now closing\n');
+  });
